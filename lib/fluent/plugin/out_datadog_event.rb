7740c9fb37d2f8260787e797d8e28e2a84766eea
inokappa <inokara@gmail.com>
2015-04-29 14:30:07 +0000

    update

--- lib/fluent/plugin/out_datadog_event.rb
+++ lib/fluent/plugin/out_datadog_event.rb
@@ -8,11 +8,11 @@ module Fluent
     config_param :app_key, :string, :default => nil
     config_param :date_happend, :string, :default => nil
     config_param :priority, :string, :default => nil
-    config_param :host, :string, :default => nil
+    # config_param :host, :string, :default => nil
     config_param :tags, :string, :default => nil
     config_param :alert_type, :string, :default => nil
     config_param :aggregation_key, :string, :default => nil
-    config_param :source_type_name, :string, :default => nil
+    config_param :source_type_name, :string, :default => 'fluentd'
 
     def configure(conf)
       super
@@ -29,26 +29,33 @@ module Fluent
       # ...
     end
 
-    def configure(conf)
-      super
-
-    end
-
     def emit(tag, es, chain)
       chain.next
       es.each do |time,record|
         record.select do |k,v|
-          event = record[k]
-          post_event(k, event)
+          event_msg = record[k]
+          post_event(k, event_msg)
         end
       end
     end
 
-    def post_event(event_key, event)
+    def post_event(event_key, event_msg)
+
       dog = Dogapi::Client.new(@api_key)
-      dog.emit_event(Dogapi::Event.new("#{event}", :msg_title => "#{event_key}"))
+      res = dog.emit_event(Dogapi::Event.new(
+        "#{event_msg}", 
+        :msg_title => "#{event_key}", 
+        :date_happend => @date_happend,
+        :priority => @priority,
+        # :host => @host,
+        :tags => @tags,
+        :alert_type => @alert_type,
+        :aggregation_key => @aggregation_key,
+        :source_type_name => @source_type_name
+      ))
       # for debug
-      # puts "debug_out: #{@api_key} - #{event_key} - #{event}\n"
+      #puts "debug_out: #{@api_key} - #{event_key} - #{event_msg} - #{options} - #{@source_type_name} \n"
+      #puts "debug_out: #{res}\n"
     end
 
   end
